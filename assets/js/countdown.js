ed5533aed48001bd668bbfb8fad4f942acdcb228
smartanalyticsla <104921918+smartanalyticsla@users.noreply.github.com>
2022-06-14 17:00:22 -0500

    Add files via upload

--- assets/js/countdown.js
+++ assets/js/countdown.js
@@ -1,8 +1,8 @@
 simplyCountdown('#cuenta', {
 	year: 2022, // required
 	month: 6, // required
-	day: 13, // required
-	hours: 22, // Default is 0 [0-23] integer
+	day: 14, // required
+	hours: 13, // Default is 0 [0-23] integer
 	minutes: 0, // Default is 0 [0-59] integer
 	seconds: 0, // Default is 0 [0-59] integer
 	words: { //words displayed into the countdown
@@ -20,7 +20,7 @@ simplyCountdown('#cuenta', {
 	onEnd: function() {
 		document.getElementById('portada').classList.add('oculta');
 		return; 
-	}, //Callback on countdown end, put your own function here
+	},//Callback on countdown end, put your own function here
 	refresh: 1000, // default refresh every 1s
 	sectionClass: 'simply-section', //section css class
 	amountClass: 'simply-amount', // amount css class
