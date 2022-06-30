f6b0f0d396aaac6f526446d4e089e619a4f39112
smartanalyticsla <104921918+smartanalyticsla@users.noreply.github.com>
2022-06-30 13:12:51 -0500

    Update countdown.js

--- assets/js/countdown.js
+++ assets/js/countdown.js
@@ -1,7 +1,7 @@
 simplyCountdown('#cuenta', {
 	year: 2022, // required
 	month: 6, // required
-	day: 29, // required
+	day: 30, // required
 	hours: 19, // Default is 0 [0-23] integer
 	minutes: 0, // Default is 0 [0-59] integer
 	seconds: 0, // Default is 0 [0-59] integer
